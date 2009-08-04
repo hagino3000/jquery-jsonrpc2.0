68bef2fe4c92a77bea9ebf0e40ae82901fc79608
Devin Torres <devin@devintorres.com>
2009-08-04 16:15:32 -0500

    * Use POST instead of GET   * Don't use JSONP   * Add success/error/fault callbacks

--- jquery.jsonrpc.js
+++ jquery.jsonrpc.js
@@ -1,32 +1,25 @@
-(function($){
+;(function($){
 
-$.jsonRpc = $.jsonRpc || function(options) {
-    options.type = options.type || 'GET';
-    var ajaxOptions = {
-        contentType: 'application/json',
-        dataType:  options.type == 'GET' ? 'jsonp' : 'json',
-        processData: options.type == 'GET'
-    };
-
-    var data = {
+  $.jsonrpc = $.jsonrpc || function(options) {
+    var options = options || {};
+    return $.ajax({
+      url: options.url || window.location.href,
+      type: options.type || 'POST',      
+      contentType: options.contentType || 'application/json',
+      dataType: options.dataType || 'json',
+      data: JSON.stringify({
         version: options.version || '1.0',
         method: options.method || 'system.listMethods',
         params: options.params || []
-    };
-    $.each(data, function(i){ delete options[i] });
-
-    function send() {
-        options.data = JSON.stringify(data);
-        if (options.type == 'GET') options.data = {json: options.data};
-        $.ajax($.extend(ajaxOptions, options));
-    }
-    
-    if (typeof JSON == 'undefined') {
-        $.getScript('http://www.json.org/json2.js', function(){ send() });
-    } else {
-        send();
-    }
-    return $;
-};
+      }),
+      success: function(resp) {
+        if (!resp.error) return options.success && options.success(resp.result);
+        else return options.fault && resp.error.message && options.fault(resp.error.message);
+      },
+      error: function(xhr, status, error) {
+        return options.error && options.error(xhr, status, error);
+      }
+    });
+  };
 
 })(jQuery);
