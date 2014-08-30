04099aee940db5d88db8c20e0704e44d0469cba3
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-08-31 00:42:10 +0900

    4 spacenized test server code.

--- test/app.js
+++ test/app.js
@@ -17,64 +17,64 @@ app.use(errorhandler({ dumpExceptions: true, showStack: true }));
 
 // Routes
 app.get('/', function(req, res){
-  res.redirect('test.html');
+    res.redirect('test.html');
 });
 
 // RPC end point
 app.post('/rpc', function(req, res) {
-  res.header('Content-Type', 'application/json');
-  var data = req.body, err = null, rpcMethod;
+    res.header('Content-Type', 'application/json');
+    var data = req.body, err = null, rpcMethod;
 
-  if (!err && data.jsonrpc !== '2.0') {
-    onError({
-      code: -32600,
-      message: 'Bad Request. JSON RPC version is invalid or missing',
-      data: null
-    }, 400);
-    return;
-  }
-
-  if (!err && !(rpcMethod = rpcMethods[data.method])) {
-    onError({
-      code: -32601,
-      message: 'Method not found : ' + data.method
-    }, 404);
-    return;
-  }
-
-  try {
-    rpcMethod(data.params, {
-      onSuccess: function(result) {
-        res.status(200).send(JSON.stringify({
-          jsonrpc: '2.0',
-          result: result,
-          id: data.id
-        }));
-      },
-      onFailure: function(error, statusCode) {
+    if (!err && data.jsonrpc !== '2.0') {
         onError({
-          code: -32603,
-          message: 'Failed',
-          data: error
-        }, statusCode || 400);
-      }
-    });
-  } catch (e) {
-    onError({
-      code: -32603,
-      message: 'Excaption at method call',
-      data: e
-    }, 500);
-  }
-  return;
+            code: -32600,
+            message: 'Bad Request. JSON RPC version is invalid or missing',
+            data: null
+        }, 400);
+        return;
+    }
 
-  function onError(err, statusCode) {
-    res.status(statusCode).send(JSON.stringify({
-      jsonrpc: '2.0',
-      error: err,
-      id: data.id
-    }));
-  }
+    if (!err && !(rpcMethod = rpcMethods[data.method])) {
+        onError({
+            code: -32601,
+            message: 'Method not found : ' + data.method
+        }, 404);
+        return;
+    }
+
+    try {
+        rpcMethod(data.params, {
+            onSuccess: function(result) {
+                res.status(200).send(JSON.stringify({
+                    jsonrpc: '2.0',
+                    result: result,
+                    id: data.id
+                }));
+            },
+            onFailure: function(error, statusCode) {
+                onError({
+                    code: -32603,
+                    message: 'Failed',
+                    data: error
+                }, statusCode || 400);
+            }
+        });
+    } catch (e) {
+        onError({
+            code: -32603,
+            message: 'Excaption at method call',
+            data: e
+        }, 500);
+    }
+    return;
+
+    function onError(err, statusCode) {
+        res.status(statusCode).send(JSON.stringify({
+            jsonrpc: '2.0',
+            error: err,
+            id: data.id
+        }));
+    }
 });
 
 app.post('/503html', function(req, res) {
--- test/methods.js
+++ test/methods.js
@@ -1,45 +1,45 @@
 function simpleMethod(params, callback) {
-  callback.onSuccess({
-    str : 'Called simpleMethod',
-    num : 1,
-    boo : true
-  });
+    callback.onSuccess({
+        str: 'Called simpleMethod',
+        num: 1,
+        boo: true
+    });
 }
 
 function throwErrorMethod(params, callback) {
-  throw 'error';
+    throw 'error';
 }
 
 function returnErrorMethod(params, callback) {
-  callback.onFailure({
-    msg : 'this is error'
-  });
+    callback.onFailure({
+        msg: 'this is error'
+    });
 }
 
 function returnErrorWith503Method(params, callback) {
-  callback.onFailure({
-    msg : 'this is error'
-  }, 503);
+    callback.onFailure({
+        msg: 'this is error'
+    }, 503);
 }
 
 function returnErrorWith200Method(params, callback) {
-  callback.onFailure({
-    msg : 'this is error but HTTP status is 200'
-  }, 200);
+    callback.onFailure({
+        msg: 'this is error but HTTP status is 200'
+    }, 200);
 }
 
 function normalMethod(params, callback) {
-  callback.onSuccess({
-    p1 : params.p1*2,
-    p2 : params.p2+'ZZZ',
-    p3 : !params.p3,
-    p4 : params.p4.map(function(n){return n*n;}),
-    p5 : params.p5
-  });
+    callback.onSuccess({
+        p1: params.p1*2,
+        p2: params.p2+'ZZZ',
+        p3: !params.p3,
+        p4: params.p4.map(function(n){return n*n;}),
+        p5: params.p5
+    });
 }
 
 function timeoutMethod(params, callback) {
-  //NOP
+    //NOP
 }
 
 
